fbaced69442668685117210e7d29038cf2b21660
Cyril COLINET <cyril.colinet@epitech.eu>
2018-03-15 20:43:30 +0100

    :tada: Add tests

--- tests/108trigo_cos_tests.c
+++ tests/108trigo_cos_tests.c
@@ -6,3 +6,11 @@
 */
 
 # include "units.h"
+
+Test(trigo, cos_example, .init = redirect_std)
+{
+	system("./108trigo COS 4 5 9 3 3 5 0 1 9");
+	cr_stdout_match_str("0.70\t-0.43\t-1.94"\
+						"-0.16\t0.67\t-1.23"\
+						"-0.06\t-0.15\t0.07");
+}
