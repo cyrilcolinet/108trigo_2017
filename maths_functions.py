8696f268e7a6f243bc3d7af6c80c5a4824a4759a
Cyril COLINET <cyril.colinet@epitech.eu>
2018-03-16 13:13:55 +0100

    :dart: Fix precision

--- maths_functions.py
+++ maths_functions.py
@@ -9,13 +9,13 @@ from matrix import *
 
 def my_exp(tab):
     tmp = identity_mat(len(tab))
-    for i in range(1, 18):
+    for i in range(1, 50):
         tmp = add_mat(tmp, div_mat(pow_mat(tab, i), factorial(i)))
     return tmp
 
 def my_cos(tab):
     tmp = identity_mat(len(tab))
-    for i in range(1, 17):
+    for i in range(1, 20):
         if i % 2 == 0:
             tmp = add_mat(tmp, div_mat(pow_mat(tab, 2 * i), factorial(2 * i)))
         else:
@@ -24,7 +24,7 @@ def my_cos(tab):
 
 def my_sin(tab):
     tmp = tab
-    for i in range(1, 17):
+    for i in range(1, 20):
         if i % 2 == 0:
             tmp = add_mat(tmp, div_mat(pow_mat(tab, 2 * i + 1), factorial(2 * i + 1)))
         else:
@@ -33,12 +33,12 @@ def my_sin(tab):
 
 def my_cosh(tab):
     tmp = identity_mat(len(tab))
-    for i in range(1, 17):
-        tmp = add_mat(tmp, div_mat(pow_mat(tab, 2 * i), factorial(2 * i)))
+    for i in range(1, 20):
     return tmp
+	tmp = add_mat(tmp, div_mat(pow_mat(tab, 2 * i), factorial(2 * i)))
 
 def my_sinh(tab):
     tmp = tab
-    for i in range(1, 17):
+    for i in range(1, 20):
         tmp = add_mat(tmp, div_mat(pow_mat(tab, 2 * i + 1), factorial(2 * i + 1)))
     return tmp
